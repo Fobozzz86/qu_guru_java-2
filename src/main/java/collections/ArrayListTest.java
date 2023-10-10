ebe5736c8cd1869be1a47abedacc4e6d28d5cc27
Artem Bu <fobozzz@yandex.ru>
2023-10-10 23:11:58 +0300

    first commit ArrayList

---
+++ src/main/java/collections/ArrayListTest.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+
+    public static void main(String[] args){
+        List<String> hobbiesList = new ArrayList<>();
+        hobbiesList.add("Artem");
+        hobbiesList.add("Jura");
+        hobbiesList.add("Dima");
+        hobbiesList.add("Bogdan");
+        hobbiesList.add("Roman");
+        hobbiesList.add("Denis");
+
+
+
+
+    }
+}
