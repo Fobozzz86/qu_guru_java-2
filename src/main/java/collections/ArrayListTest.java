26fec68c7f5f94ba2f3b105d688d692941a5991d
Artem Bu <fobozzz@yandex.ru>
2023-10-10 23:59:08 +0300

    ArrayList Done

--- src/main/java/collections/ArrayListTest.java
+++ src/main/java/collections/ArrayListTest.java
@@ -6,16 +6,26 @@ import java.util.List;
 public class ArrayListTest {
 
     public static void main(String[] args){
-        List<String> hobbiesList = new ArrayList<>();
-        hobbiesList.add("Artem");
-        hobbiesList.add("Jura");
-        hobbiesList.add("Dima");
-        hobbiesList.add("Bogdan");
-        hobbiesList.add("Roman");
-        hobbiesList.add("Denis");
+        List<String> nameList = new ArrayList<>();
+        nameList.add("Artem");
+        nameList.add("Jura");
+        nameList.add("Dima");
+        nameList.add("Bogdan");
+        nameList.add("Roman");
+        nameList.add("Denis");
 
+        nameList.remove(1);
 
+        nameList.add(1, "Jorik");
 
+        for (int i = 0; i < nameList.size(); i++) {
 
+            if (nameList.get(i).contains("Denis")) {
+                System.out.println(nameList.get(0) + " " + "ну точно Артём");
+                break;
+
+            } else System.out.println(nameList);
+                break;
+        }
     }
 }
